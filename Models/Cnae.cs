16b0a3b05d7bfebfeef14a7498222b44f3a35dbe
Paulinely Morgan <paulinelym@gmail.com>
2020-02-21 17:35:50 -0300

    decoracao [key] para o Id do Model

--- Models/Cnae.cs
+++ Models/Cnae.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace utools.Models
 {
     public class Cnae
     {
+        [Key]
         public int Id { get; set; }
         public string code { get; set; }
         public string text { get; set; }
