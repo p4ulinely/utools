7c6e283dc4fae22efc72a5d7976c2719d68e871e
Paulinely Morgan <paulinelym@gmail.com>
2020-02-21 17:41:48 -0300

    pequenas adequacoes

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ namespace utools
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
+            // migrations (ou descomenta ou faz o migration manual (dotnet ef database update))
+            // new DataContext().Database.Migrate();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Startup.cs
+++ Startup.cs
@@ -16,23 +16,25 @@ namespace utools
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddCors();
             services.AddDbContext<DataContext>();
             services.AddScoped<DataContext, DataContext>(); // garante apenas um contexto, evitando lixo de memória
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -42,6 +44,7 @@ namespace utools
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
